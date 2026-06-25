a788e4914f4bd8952ec175f266bee7fefa30d663
git config --global user.name <tamilarasan.softwarengineer@gmail.com>
2026-06-25 10:02:37 +0530

    updates

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -15,6 +15,7 @@ export default function Header() {
   const navLinks = [
     { name: 'Home', href: '/' },
     { name: 'Gallery', href: '/gallery' },
+    { name: 'Video', href: '/video' },
     {name:'About', href:'/about'},
     { name: 'Contact', href: '/contact' },
   ];
